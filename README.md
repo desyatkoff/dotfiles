b09f5c4149205f2395868e6c0efd898d4b649e2d
Desyatkov Sergey <sergeidesyatkov@gmail.com>
2025-04-04 22:45:02 +0500

    Update

--- README.md
+++ README.md
@@ -5,5 +5,5 @@
 This repository stores my dotfiles that I personally use at the moment. It's very very minimal (almost default) and you can use this as a base config
 
 ## Screenshots
-![](https://github.com/desyatkoff/dotfiles/demo/screenshot.png)
+![](/demo/screenshot.png)
 
--- install.sh
+++ install.sh
@@ -1,8 +1,12 @@
 #!/usr/bin/env bash
 
-mv ~/.config/hypr/ ~/.config/hypr-old/
+mv ~/.config/alacritty/ ~/.config/alacritty-old/
 mv ~/.config/fastfetch/ ~/.config/fastfetch-old/
+mv ~/.config/hypr/ ~/.config/hypr-old/
+mv ~/.config/wofi/ ~/.config/wofi-old/
 
-cp -r hypr/* ~/.config/hypr/
+cp -r alacritty/* ~/.config/alacritty/
 cp -r fastfetch/* ~/.config/fastfetch/
+cp -r hypr/* ~/.config/hypr/
+cp -r wofi/* ~/.config/wofi/
 
