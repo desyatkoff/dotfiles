9231d709f4bbb55f63a9152bbe64dc5cd76895c4
Desyatkov Sergey <sergeidesyatkov@gmail.com>
2025-04-05 10:13:01 +0500

    Update

--- install.sh
+++ install.sh
@@ -1,12 +1,43 @@
 #!/usr/bin/env bash
 
-mv ~/.config/alacritty/ ~/.config/alacritty-old/
-mv ~/.config/fastfetch/ ~/.config/fastfetch-old/
-mv ~/.config/hypr/ ~/.config/hypr-old/
-mv ~/.config/wofi/ ~/.config/wofi-old/
 
-cp alacritty/* ~/.config/alacritty/
-cp fastfetch/* ~/.config/fastfetch/
-cp hypr/* ~/.config/hypr/
-cp wofi/* ~/.config/wofi/
+if [ -d ~/.config/alacritty/ ]; then
+    mv -v ~/.config/alacritty/ \
+        ~/.config/alacritty-old/
+fi
+
+if [ -d ~/.config/fastfetch/ ]; then
+    mv -v ~/.config/fastfetch/ \
+        ~/.config/fastfetch-old/
+fi
+
+if [ -d ~/.config/hypr/ ]; then
+    mv -v ~/.config/hypr/ \
+        ~/.config/hypr-old/
+fi
+
+if [ -d ~/.config/wofi/ ]; then
+    mv -v ~/.config/wofi/ \
+        ~/.config/wofi-old/
+fi
+
+mkdir -v ~/.config/alacritty/
+
+mkdir -v ~/.config/fastfetch/
+
+mkdir -v ~/.config/hypr/
+
+mkdir -v ~/.config/wofi/
+
+cp -v alacritty/* \
+    ~/.config/alacritty/
+
+cp -v fastfetch/* \
+    ~/.config/fastfetch/
+
+cp -v hypr/* \
+    ~/.config/hypr/
+
+cp -v wofi/* \
+    ~/.config/wofi/
 
