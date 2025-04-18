1aae1096ea5b63d1d466b3052e93d906d310a227
Desyatkov Sergey <sergeidesyatkov@gmail.com>
2025-04-18 19:49:36 +0500

    Update

--- install.sh
+++ install.sh
@@ -17,7 +17,9 @@
 # 1. Install required packages (if you change something, you should edit all the configs as well)
 
 sudo pacman -S --needed \
-    grimblast \
+    grim \
+    slurp \
+    swappy \
     hyprpaper \
     cava \
     fastfetch \
@@ -34,42 +36,50 @@ sudo pacman -S --needed \
 # 2. Rename old configs so you can restore your old settings if needed
 
 if [ -d ~/.config/cava/ ]; then
-    mv -v ~/.config/cava/ \
+    mv -v \
+        ~/.config/cava/ \
         ~/.config/cava-old/
 fi
 
 if [ -d ~/.config/fastfetch/ ]; then
-    mv -v ~/.config/fastfetch/ \
+    mv -v \
+        ~/.config/fastfetch/ \
         ~/.config/fastfetch-old/
 fi
 
 if [ -d ~/.config/helix/ ]; then
-    mv -v ~/.config/helix/ \
+    mv -v \
+        ~/.config/helix/ \
         ~/.config/helix-old/
 fi
 
 if [ -d ~/.config/hypr/ ]; then
-    mv -v ~/.config/hypr/ \
+    mv -v \
+        ~/.config/hypr/ \
         ~/.config/hypr-old/
 fi
 
 if [ -d ~/.config/kitty/ ]; then
-    mv -v ~/.config/kitty/ \
+    mv -v \
+        ~/.config/kitty/ \
         ~/.config/kitty-old/
 fi
 
 if [ -d ~/.config/mako/ ]; then
-    mv -v ~/.config/mako/ \
+    mv -v \
+        ~/.config/mako/ \
         ~/.config/mako-old/
 fi
 
 if [ -d ~/.config/peaclock/ ]; then
-    mv -v ~/.config/peaclock/ \
+    mv -v \
+        ~/.config/peaclock/ \
         ~/.config/peaclock-old/
 fi
 
 if [ -d ~/.config/wofi/ ]; then
-    mv -v ~/.config/wofi/ \
+    mv -v \
+        ~/.config/wofi/ \
         ~/.config/wofi-old/
 fi
 
@@ -95,28 +105,36 @@ mkdir -v ~/.config/wofi/
 
 # 4. Copy the new configs
 
-cp -v cava/* \
+cp -v \
+    cava/* \
     ~/.config/cava/
 
-cp -v fastfetch/* \
+cp -v \
+    fastfetch/* \
     ~/.config/fastfetch/
 
-cp -v helix/* \
+cp -v \
+    helix/* \
     ~/.config/helix/
 
-cp -v hypr/* \
+cp -v \
+    hypr/* \
     ~/.config/hypr/
 
-cp -v kitty/* \
+cp -v \
+    kitty/* \
     ~/.config/kitty/
 
-cp -v mako/* \
+cp -v \
+    mako/* \
     ~/.config/mako/
 
-cp -v peaclock/* \
+cp -v \
+    peaclock/* \
     ~/.config/peaclock/
 
-cp -v wofi/* \
+cp -v \
+    wofi/* \
     ~/.config/wofi/
 
 
